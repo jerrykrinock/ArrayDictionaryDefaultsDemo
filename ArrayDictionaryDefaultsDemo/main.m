0ad9c3489d5a66f5313d62863e1cef47b401d57a
Jerry Krinock <jerry@sheepsystems.com>
2016-05-04 12:42:33 -0700

    Initial Commit

---
+++ ArrayDictionaryDefaultsDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ArrayDictionaryDefaultsDemo
+//
+//  Created by Jerry on 16/05/04.
+//  Copyright © 2016 Jerry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
